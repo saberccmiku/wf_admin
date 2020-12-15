581fa4deb19347d0444b4c2c79847fe692af74b9
saber <639878266@qq.com>
2020-12-15 09:33:02 +0800

    修改了model

--- src/views/model/model-list.vue
+++ src/views/model/model-list.vue
@@ -98,7 +98,7 @@
       </el-table-column>
     </el-table>
 
-    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.current" :limit.sync="listQuery.limit" @pagination="getList" />
   </div>
 </template>
 
